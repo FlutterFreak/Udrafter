18bfe414f5ca71bd4dc82c060891543d983dcb25
a <a@a.com>
2017-03-28 14:32:23 +0100

    job post

--- app/search.php
+++ app/search.php
@@ -3,35 +3,36 @@
 
 session_start();
 
-if (isset ($_GET["submit"])){
+if (isset ($_GET["submit"])) {
     if (isset($_GET["title"])) {
         $Title = $_GET["title"];
         echo "<p> " . $Title . "</p> ";
-    }}
-    else {
+    } else {
         echo "<p>Please Enter a Search Query</p>";
     }
 //connect to database 
-        include 'db_connect.php';
-        $query = "SELECT * from Job WHERE title LIKE '%" . $Title . "%' OR description LIKE '%" . $Title . "%'";
+    include 'db_connect.php';
+    $query = "SELECT * from Job WHERE title LIKE '%" . $Title . "%' OR description LIKE '%" . $Title . "%'";
 
 
-        $results = $connection->query($query);
+    $results = $connection->query($query);
 
 
 // create while loop and loop through results 
-        while ($row = mysql_fetch_array($results)) {
-            $title = $row["title"];
-            $description = $row["description"];
-            $category = $row["category"];
-            $wages = $row["wages"];
-            $company = $row["company"];
-            $location = $row["location"];
-            $date = $row["date"];
+    while ($row = mysql_fetch_array($results)) {
+        $title = $row["title"];
+        $description = $row["description"];
+        $category = $row["category"];
+        $wages = $row["wages"];
+        $company = $row["company"];
+        $location = $row["location"];
+        $date = $row["date"];
 //display the results   
-            echo "<ul>\n";
-            echo "<li>" . $title . " " . $description . " " . $category . " " . $wages . " " . $company . " " . $location . " " . $date . "</li>\n";
-            echo "</ul>";
-        }
-
+        echo "<ul>\n";
+        echo "<li>" . $title . " " . $description . " " . $category . " " . $wages . " " . $company . " " . $location . " " . $date . "</li>\n";
+        echo "</ul>";
+    }
+} else{
+    echo "<p>No results Found</p>";
+}
 
