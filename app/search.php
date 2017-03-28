442ec07dc5f2de5713f544fbc77ed5f9c90e6e51
a <a@a.com>
2017-03-28 14:28:37 +0100

    job post

--- app/search.php
+++ app/search.php
@@ -4,35 +4,34 @@
 session_start();
 
 if (isset ($_GET["submit"])){
-    if (isset($_GET["title"])){
+    if (isset($_GET["title"])) {
         $Title = $_GET["title"];
-        echo "<p> " .$Title ."</p> ";
+        echo "<p> " . $Title . "</p> ";
+    }}
+    else {
+        echo "<p>Please Enter a Search Query</p>";
+    }
 //connect to database 
         include 'db_connect.php';
-        $query= "SELECT * from Job WHERE title LIKE '%" .$Title. "%' OR description LIKE '%" .$Title. "%'";
-        
-       
+        $query = "SELECT * from Job WHERE title LIKE '%" . $Title . "%' OR description LIKE '%" . $Title . "%'";
 
 
-        $results = $connection->query ($query);
+        $results = $connection->query($query);
 
-      
+
 // create while loop and loop through results 
         while ($row = mysql_fetch_array($results)) {
             $title = $row["title"];
             $description = $row["description"];
             $category = $row["category"];
             $wages = $row["wages"];
-            $company= $row["company"];
+            $company = $row["company"];
             $location = $row["location"];
             $date = $row["date"];
 //display the results   
             echo "<ul>\n";
-            echo "<li>". $title. " ".$description." "    .$category." " .$wages." " . $company. " " . $location. " " .$date."</li>\n"; 
-echo "</ul>";   
-}
-    }
-}
-else {
-    echo "<p>Please Enter a Search Query</p>";
-}
+            echo "<li>" . $title . " " . $description . " " . $category . " " . $wages . " " . $company . " " . $location . " " . $date . "</li>\n";
+            echo "</ul>";
+        }
+
+
