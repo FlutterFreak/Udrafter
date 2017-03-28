4b717e0238ca18b4d7b514cd2cae7de9331dd44f
a <a@a.com>
2017-03-28 17:26:50 +0100

    search

--- app/search.php
+++ app/search.php
@@ -23,7 +23,7 @@ echo "<p> xxx  $Title</p> ";
 
 $jobId = $row["jobId"];
 $description = $row["description"];
-echo "<p> xxx $jobId</p>";
+echo "<p> xxx $description</p>";
 // create while loop and loop through results 
     while ($row = mysql_fetch_array($results)) {
         $title = $row["title"];
