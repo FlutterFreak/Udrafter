b13e8783af1b8a55ebd47b3fe339d578d8f03373
a <a@a.com>
2017-03-29 15:23:35 +0100

    edit

--- app/jobs.php
+++ app/jobs.php
@@ -1,4 +1,5 @@
 <?
+session_start();
 
 include 'db_connect.php';
 
@@ -14,7 +15,7 @@ for ($i = 0; $i < $num_results; $i++) {
     $row = mysqli_fetch_array($results);
 
         $jobId = $row["jobId"];
-        $employerId = $row["employerId"]
+        $employerId = $row["employerId"];
         $title = $row["title"];
         $description = $row["description"];
         $category = $row["category"];
