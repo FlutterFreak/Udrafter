5aa9f1896639d48c52cc26afbfdb144defba0f64
a <a@a.com>
2017-03-29 15:19:28 +0100

    edit

--- app/jobs.php
+++ app/jobs.php
@@ -13,9 +13,17 @@ echo  $num_results;
 for ($i = 0; $i < $num_results; $i++) {
     $row = mysqli_fetch_array($results);
 
-       $jobId = $row["jobId"];
+        $jobId = $row["jobId"];
+        $employerId = $row["employerId"]
+        $title = $row["title"];
+        $description = $row["description"];
+        $category = $row["category"];
+        $wages = $row["wages"];
+        $company = $row["company"];
+        $location = $row["location"];
+        $date = $row["date"];
 
-       echo $jobid;
+       echo $employerId;
 
 
 
