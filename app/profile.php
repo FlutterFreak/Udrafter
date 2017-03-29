5663fb146a110ba996681b89b434df14460826f9
a <a@a.com>
2017-03-29 12:54:40 +0100

    search

--- app/profile.php
+++ app/profile.php
@@ -1,7 +1,14 @@
 <?php
 session_start();
+?>
 
+<html>
+<head>
+    <title>Employer Registration</title>
+</head>
+<body>
 
+<?
 if (isset ($_SESSION["email"])) {
     $email = $_SESSION["email"];
 
@@ -19,29 +26,13 @@ $row = mysqli_fetch_array($results);
     $profilePic= $row["profilePic"];
 
 }
+echo "<p>Your Profile Information</p>";
+echo "<p><img src= $profilePic </p>";
 
+echo "<p>Name:" . $name ."</p>";
+echo "<p>Email:" . $email ."</p>";
+echo "<p>Company:" . $company ."</p>";
 ?>
 
 
- <table width="398" border="0" align="center" cellpadding="0">
-    <tr>
-        <td height="26" colspan="2">Your Profile Information </td>
-        <td><div align="right"><a href="logout.php">logout</a></div></td>
-    </tr>
-    <tr>
-        <td width="129" rowspan="5"><img src="<?php echo $profilePic ?>" width="129" height="129" alt="no image found"/></td>
-        <td width="82" valign="top"><div align="left">Name:</div></td>
-        <td width="165" valign="top"><?php echo $name ?></td>
-    </tr>
-    <tr>
-        <td valign="top"><div align="left">Email:</div></td>
-        <td valign="top"><?php echo $email ?></td>
-    </tr>
-    <tr>
-        <td valign="top"><div align="left">Company:</div></td>
-        <td valign="top"><?php echo $company ?></td>
-    </tr>
-
-</table>
-<p align="center"><a href="index.php"></a></p>
-
+ 
