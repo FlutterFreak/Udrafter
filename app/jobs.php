51514c8270e5a0410f620098f86a778ccd45a2c5
a <a@a.com>
2017-03-29 15:11:08 +0100

    edit

--- app/jobs.php
+++ app/jobs.php
@@ -7,7 +7,7 @@ $query_get = "select * from Jobs";
 $results = $connection->query ($query_get);
 
 $num_results = mysqli_num_rows ($results);
-echo $num_results;
+echo "xx". $num_results;
 
 for ($i = 0; $i < $num_results; $i++) {
    while( $row = mysqli_fetch_array($results)){
