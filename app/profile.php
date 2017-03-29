ef7e11009375fe33bb2d4dd6bbec9bf94eebf110
a <a@a.com>
2017-03-29 13:01:01 +0100

    search

--- app/profile.php
+++ app/profile.php
@@ -12,27 +12,50 @@ session_start();
 if (isset ($_SESSION["email"])) {
     $email = $_SESSION["email"];
 
-include 'db_connect.php';
-$query_get = "select * from Employer where  email=\"$email\"";
+    include 'db_connect.php';
+    $query_get = "select * from Employer where  email=\"$email\"";
 
-$results = $connection->query ($query_get);
+    $results = $connection->query($query_get);
 
 
-$row = mysqli_fetch_array($results);
+    $row = mysqli_fetch_array($results);
 
     $name = $row["name"];
     $email = $row["email"];
     $company = $row["company"];
-    $profilePic= $row["profilePic"];
+    $profilePic = $row["profilePic"];
+
+
+    echo "<p>Your Profile Information</p>";
+    echo "<p><img src= $profilePic </p>";
+
+    echo "<p>Name:" . $name . "</p>";
+    echo "<p>Email:" . $email . "</p>";
+    echo "<p>Company:" . $company . "</p>";
+}
+else if (isset ($_SESSION["uniEmail"])) {
+    $uniEmail = $_SESSION["uniEmail"];
+    include 'db_connect.php';
+
+    $query_get = "select * from Student where  uniEmail=\"$uniEmail\"";
+
+    $results = $connection->query($query_get);
+
+
+    $row = mysqli_fetch_array($results);
+
+    $name = $row["name"];
+
+    $uniEmail = $row["uniEmail"];
+    $profilePic = $row["profilePic"];
+
+    echo "<p>Your Profile Information</p>";
+    echo "<p><img src= $profilePic </p>";
+
+    echo "<p>Name:" . $name . "</p>";
+    echo "<p> University Email:" . $uniEmail . "</p>";
 
 }
-echo "<p>Your Profile Information</p>";
-echo "<p><img src= $profilePic </p>";
-
-echo "<p>Name:" . $name ."</p>";
-echo "<p>Email:" . $email ."</p>";
-echo "<p>Company:" . $company ."</p>";
 ?>
 
 
- 
