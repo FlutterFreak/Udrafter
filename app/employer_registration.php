4f9394ea282976961a21ba352ca09a6ed48715c2
a <a@a.com>
2017-05-02 13:50:07 +0100

    hgv

--- app/employer_registration.php
+++ app/employer_registration.php
@@ -4,7 +4,7 @@ session_start();
 
 
 <?php
-// http://udrafter2017.azurewebsites.net
+
 header('Content-type: application/json');
 
 if (isset ($_POST["name"])) {
@@ -20,45 +20,58 @@ if (isset ($_POST["company"])) {
     $company = $_POST["company"];
 }
 
- if(!empty($name) && !empty($email) && !empty($password) && !empty($company)) {
+if(!empty($name) && !empty($email) && !empty($password) && !empty($company)){
 
-     $hashed_password = password_hash($password, PASSWORD_DEFAULT); //here i am hashing the password
-
-     include 'db_connect.php';
-
-     $query_check = "select * from Employer where email=\"$email\"";
-
-     $results = $connection->query($query_check);
-
-     if (!$results) {
-         $json["error"] = mysql_error($connection);
-         echo json_encode($json);
-     }
-
-     $num_results = mysqli_num_rows($results);
-
-     if ($num_results = 0) {
-
-         $query = "insert into Employer (name, password, email, company) values (\"$name\", \"$hashed_password\",\"$email\",\"$company\")";
-
-         $ret = $connection->query($query);
-
-
-         if (!$ret) {
-
-             $json["error"] = mysql_error($connection);
-             echo json_encode($json);
-         }
-
-         $_SESSION["email"] = $email;
-
-         $response["success"] = 'Registration Sucessfull' . "" . $email;
-
-     }
- }
+ $hashed_password = password_hash($password, PASSWORD_DEFAULT); //here i am hashing the password
 
 
 
+include 'db_connect.php';
+
+$query_check = "select * from Employer where email=\"$email\"";
+
+$results = $connection->query ($query_check);
+
+if (!$results) {
+    $json["error"] =  mysql_error($connection);
+    echo json_encode($json);
+}
+
+$num_results = mysqli_num_rows ($results);
+
+if ($num_results != 0) {
+   // user already exists
+    // failed
+ $response["failed"] = 'User Already Exists';
+ // echoing JSON response
+  echo json_encode($response);
+}
+
+$query = "insert into Employer (name, password, email, company) values (\"$name\", \"$hashed_password\",\"$email\",\"$company\")";
+
+$ret = $connection->query ($query);
+
+
+if (!$ret) {
+
+    $json["error"] =  mysql_error($connection);
+    echo json_encode($json);
+}
+
+    $_SESSION["email"] = $email;
+
+// success
+
+$response["success"] = 'Registration Sucessfull' . $email;
+
+// echoing JSON response
+
+echo json_encode($response);
+
+}
+else {
+    echo json_encode('Please provide all Fields');
+}
 
 ?>
 
