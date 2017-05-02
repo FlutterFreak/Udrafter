3935345ee30aa8cab6639f1657a02538a24e827e
a <a@a.com>
2017-05-02 12:59:43 +0100

    iuy

--- app/employer_registration.php
+++ app/employer_registration.php
@@ -8,16 +8,16 @@ session_start();
 header('Content-type: application/json');
 
 if (isset ($_POST["name"])) {
-    $name1 = $_POST["name"];
+    $name = $_POST["name"];
 }
 if (isset ($_POST["email"])) {
-    $email1 = $_POST["email"];
+    $email = $_POST["email"];
 }
 if (isset ($_POST["password"])) {
-    $password1= $_POST["password"];
+    $password= $_POST["password"];
 }
 if (isset ($_POST["company"])) {
-    $company1 = $_POST["company"];
+    $company = $_POST["company"];
 }
 
 //if(!empty($name) && !empty($email) && !empty($password) && !empty($company)){
