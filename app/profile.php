f171f8a1ff650e67548d511d87d2684ae27b6ea4
laptop <n.kulhar@rgu.ac.uk>
2017-05-09 09:18:34 +0100

    json

--- app/profile.php
+++ app/profile.php
@@ -16,16 +16,19 @@ if (isset ($_SESSION["email"])) {
     $email = $row["email"];
     $company = $row["company"];
     $profilePic = $row["profilePic"];
+    
+    $num_results = mysqli_num_rows ($results);
+
+    $encode = array();
+
+    while($row = mysqli_fetch_assoc($results)) {
+        $encode[] = $row;
+    }
+
+    echo json_encode($encode);
 
 
-    echo "<p>Your Profile Information</p>";
-    echo "<p><img src= $profilePic </p>";
 
-    echo "<p>Name:" . $name . "</p>";
-    echo "<p>Email:" . $email . "</p>";
-    echo "<p>Company:" . $company . "</p>";
-
-    echo "<p>Click <a href = \"edit_employer.html\">here</a>  to edit your profile.</p>";
 }
 else if (isset ($_SESSION["uniemail"])) {
     $uniemail = $_SESSION["uniemail"];
