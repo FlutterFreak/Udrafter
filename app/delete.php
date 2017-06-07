8212841724c09d3b9d93b545f70451adf832f87d
laptop <n.kulhar@rgu.ac.uk>
2017-06-07 07:10:49 +0100

    json

---
+++ app/delete.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+include 'db_connect.php';
+header('Content-type: application/json');
+if (isset ($_SESSION["email"])) {
+    $email = $_SESSION["email"];
+
+    $query_get ="DELETE FROM Employer WHERE email=\"$email\"";
+    $results = $connection->query($query_check);
+    if($results== true){
+        $response["deleted"] = 'Account Successfully Deleted';
+        // echoing JSON response
+        echo json_encode($response);
+        session_destroy();
+        exit;
+    }
+
+}else if (isset ($_SESSION["uniemail"])) {
+    $uniemail = $_SESSION["uniemail"];
+
+    $query_get ="DELETE FROM Student WHERE uniEmail=\"$uniemail\"";
+    $results = $connection->query($query_check);
+    if($results== true){
+        $response["deleted"] = 'Account Successfully Deleted';
+        // echoing JSON response
+        echo json_encode($response);
+        session_destroy();
+        exit;
+}}
+$connection->close();
+
+?>
