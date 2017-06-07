35e2f47686a45083195d6d1c386c886abf086140
laptop <n.kulhar@rgu.ac.uk>
2017-06-07 01:01:28 +0100

    json

--- app/apply.php
+++ app/apply.php
@@ -9,7 +9,7 @@ session_start();
     echo json_encode($json);
     return;
 }*/
-    
+
     $uniemail= $_SESSION["uniemail"];
 
 if (isset ($_GET["jobId"])) {
@@ -26,6 +26,24 @@ if (isset ($_GET["employerId"])) {
 
     $row = mysqli_fetch_array($results);
     $studentId = $row["studentId"];
+
+$query_check = "select * from Student where jobId=\"$jobId\"";
+
+$results = $connection->query($query_check);
+
+if (!$results) {
+    $response["failed"] = mysql_error($connection);
+    echo json_encode($response);
+}
+
+$num_results = mysqli_num_rows($results);
+
+if ($num_results != 0) {
+    $response["applied"] = 'Yo have already applied for this job';
+    // echoing JSON response
+    echo json_encode($response);
+    exit;
+}
 
 $query = "insert into Application ( jobId, studentId, employerId) values($jobId, \"$studentId\",\"$employerId\")";
     
