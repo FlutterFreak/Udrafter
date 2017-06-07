40545eed186676bed251fb755742b3781712336b
laptop <n.kulhar@rgu.ac.uk>
2017-06-07 03:04:27 +0100

    json

--- app/jobsApplied.php
+++ app/jobsApplied.php
@@ -1,11 +1,12 @@
 <?php
-session_start();
+//session_start();
 //Jobs Applied
 include 'db_connect.php';
 
 header('Content-type: application/json');
 
-$uniemail= $_SESSION["uniemail"];
+//$uniemail= $_SESSION["uniemail"];
+$uniemail= "abc@rgu.ac.uk";
 
 $query_get = "select * from Student where  uniEmail=\"$uniemail\"";
 
@@ -19,18 +20,18 @@ $query_check = "select jobId from Application where studentId=\"$studentId\"";
 $results = $connection->query($query_check);
 
 $num_results = mysqli_num_rows($results);
-
+$encode = array();
 for ($i = 0; $i < $num_results; $i++) {
     $row = mysqli_fetch_array($results);
 
     $jobId = $row["jobId"];
     $query_get = "select * from Job where  jobId=\"$jobId\"";
-    $results = $connection->query ($query_get);
-    $encode = array();
+    $result = $connection->query ($query_get);
 
-    while($row = mysqli_fetch_assoc($results)) {
+
+    $row = mysqli_fetch_assoc($result);
         array_push ($encode, $row);
-    }
+
 }
 
 echo json_encode($encode);
