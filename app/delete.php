2ea1a2c24e33e68b05cfeb105add77ce6b635e79
laptop <n.kulhar@rgu.ac.uk>
2017-06-07 07:33:55 +0100

    json

--- app/delete.php
+++ app/delete.php
@@ -4,7 +4,7 @@ include 'db_connect.php';
 header('Content-type: application/json');
 if (isset ($_SESSION["email"])) {
     $email = $_SESSION["email"];
-
+echo "jhf".$email;
     $query_get ="delete * from Employer where email=\"$email\"";
     $results = $connection->query($query_check);
     if($results== true){
